54f8ba068de98e72bc956dfff0c98b0b0c3440b6
Shadman Ahmed <shadman@outlook.com>
2020-04-12 01:41:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
-  index = input_to_index(input)
+  input = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
